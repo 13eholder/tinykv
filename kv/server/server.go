e559ffaddf4bdcf5f85fc52d5f60e51b6c136adb
13eholder <925349624@qq.com>
2025-04-01 17:12:07 +0800

    feat: impl 4c and tests passed

--- kv/server/server.go
+++ kv/server/server.go
@@ -9,6 +9,7 @@ import (
 	"github.com/pingcap-incubator/tinykv/kv/storage/raft_storage"
 	"github.com/pingcap-incubator/tinykv/kv/transaction/latches"
 	"github.com/pingcap-incubator/tinykv/kv/transaction/mvcc"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 	coppb "github.com/pingcap-incubator/tinykv/proto/pkg/coprocessor"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/tinykvpb"
@@ -212,22 +213,167 @@ func (server *Server) KvCommit(_ context.Context, req *kvrpcpb.CommitRequest) (*
 
 func (server *Server) KvScan(_ context.Context, req *kvrpcpb.ScanRequest) (*kvrpcpb.ScanResponse, error) {
 	// Your Code Here (4C).
-	return nil, nil
+	resp := new(kvrpcpb.ScanResponse)
+
+	reader, _ := server.storage.Reader(req.Context)
+	mvccTxn := mvcc.NewMvccTxn(reader, req.Version)
+	scanner := mvcc.NewScanner(req.StartKey, mvccTxn)
+	defer scanner.Close()
+
+	kvPairs := make([]*kvrpcpb.KvPair, 0)
+	for i := 0; i < int(req.Limit); i++ {
+		key, value, _ := scanner.Next()
+		if key == nil && value == nil {
+			break
+		}
+		kv := &kvrpcpb.KvPair{}
+		lock, _ := mvccTxn.GetLock(key)
+		if lock != nil && lock.IsLockedFor(key, req.Version, kv) {
+			kvPairs = append(kvPairs, kv)
+			continue
+		}
+		if value != nil {
+			kv.Key = key
+			kv.Value = value
+			kvPairs = append(kvPairs, kv)
+		}
+	}
+	resp.Pairs = kvPairs
+	return resp, nil
 }
 
 func (server *Server) KvCheckTxnStatus(_ context.Context, req *kvrpcpb.CheckTxnStatusRequest) (*kvrpcpb.CheckTxnStatusResponse, error) {
 	// Your Code Here (4C).
-	return nil, nil
+	resp := new(kvrpcpb.CheckTxnStatusResponse)
+
+	reader, _ := server.storage.Reader(req.Context)
+	mvccTxn := mvcc.NewMvccTxn(reader, req.LockTs)
+
+	// 检测是否提交,提交的话就忽略
+	write, commitTs, _ := mvccTxn.CurrentWrite(req.PrimaryKey)
+	if write != nil && write.Kind != mvcc.WriteKindRollback {
+		resp.CommitVersion = commitTs
+		return resp, nil
+	}
+
+	lock, _ := mvccTxn.GetLock(req.PrimaryKey)
+
+	if lock == nil {
+		if write != nil && write.Kind == mvcc.WriteKindRollback {
+			resp.Action = kvrpcpb.Action_NoAction
+			return resp, nil
+		}
+		mvccTxn.PutWrite(req.PrimaryKey, req.LockTs, &mvcc.Write{
+			StartTS: req.LockTs,
+			Kind:    mvcc.WriteKindRollback,
+		})
+		server.storage.Write(req.Context, mvccTxn.Writes())
+		resp.Action = kvrpcpb.Action_LockNotExistRollback
+		return resp, nil
+	}
+	curTime := mvcc.PhysicalTime(req.CurrentTs)
+	lockTime := mvcc.PhysicalTime(lock.Ts)
+
+	if curTime >= lockTime && curTime-lockTime >= lock.Ttl {
+		mvccTxn.DeleteLock(req.PrimaryKey)
+		mvccTxn.DeleteValue(req.PrimaryKey)
+		mvccTxn.PutWrite(req.PrimaryKey, req.LockTs, &mvcc.Write{
+			StartTS: req.LockTs,
+			Kind:    mvcc.WriteKindRollback,
+		})
+		resp.Action = kvrpcpb.Action_TTLExpireRollback
+	} else {
+		resp.Action = kvrpcpb.Action_NoAction
+	}
+	server.storage.Write(req.Context, mvccTxn.Writes())
+	return resp, nil
 }
 
+// 检查密钥是否被当前事务锁定，如果是，则删除锁定，删除值并将回滚指示器保留为写入
 func (server *Server) KvBatchRollback(_ context.Context, req *kvrpcpb.BatchRollbackRequest) (*kvrpcpb.BatchRollbackResponse, error) {
 	// Your Code Here (4C).
-	return nil, nil
+	server.Latches.WaitForLatches(req.Keys)
+	defer server.Latches.ReleaseLatches(req.Keys)
+
+	resp := new(kvrpcpb.BatchRollbackResponse)
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		return resp, err
+	}
+	mvccTxn := mvcc.NewMvccTxn(reader, req.StartVersion)
+	for _, key := range req.Keys {
+		lock, _ := mvccTxn.GetLock(key)
+		if lock != nil && lock.Ts == req.StartVersion {
+			mvccTxn.DeleteLock(key)
+			mvccTxn.DeleteValue(key)
+			mvccTxn.PutWrite(key, req.StartVersion, &mvcc.Write{
+				StartTS: req.StartVersion,
+				Kind:    mvcc.WriteKindRollback,
+			})
+		} else {
+			// 如果已被删除,并且没有回滚指示器,则添加
+			write, _, _ := mvccTxn.CurrentWrite(key)
+			if write == nil {
+				mvccTxn.PutWrite(key, req.StartVersion, &mvcc.Write{
+					StartTS: req.StartVersion,
+					Kind:    mvcc.WriteKindRollback,
+				})
+			} else if write.StartTS == req.StartVersion && write.Kind != mvcc.WriteKindRollback {
+				resp.Error = &kvrpcpb.KeyError{
+					Abort: "true",
+				}
+			}
+		}
+	}
+	err = server.storage.Write(req.Context, mvccTxn.Writes())
+	if err != nil {
+		return resp, err
+	}
+	return resp, nil
 }
 
 func (server *Server) KvResolveLock(_ context.Context, req *kvrpcpb.ResolveLockRequest) (*kvrpcpb.ResolveLockResponse, error) {
 	// Your Code Here (4C).
-	return nil, nil
+	resp := new(kvrpcpb.ResolveLockResponse)
+
+	reader, _ := server.storage.Reader(req.Context)
+	mvccTxn := mvcc.NewMvccTxn(reader, req.StartVersion)
+
+	var keys [][]byte
+	iter := mvccTxn.Reader.IterCF(engine_util.CfLock)
+	defer iter.Close()
+	for ; iter.Valid(); iter.Next() {
+		item := iter.Item()
+		value, _ := item.Value()
+		lock, _ := mvcc.ParseLock(value)
+		if lock.Ts == req.StartVersion {
+			keys = append(keys, item.Key())
+		}
+	}
+
+	if req.CommitVersion == 0 {
+		// 全部回滚
+		rb := &kvrpcpb.BatchRollbackRequest{
+			Context:      req.Context,
+			StartVersion: req.StartVersion,
+			Keys:         keys,
+		}
+		rbResp, _ := server.KvBatchRollback(context.TODO(), rb)
+		resp.Error = rbResp.Error
+		resp.RegionError = rbResp.RegionError
+	} else {
+		commit := &kvrpcpb.CommitRequest{
+			Context:       req.Context,
+			StartVersion:  req.StartVersion,
+			CommitVersion: req.CommitVersion,
+			Keys:          keys,
+		}
+		commitResp, _ := server.KvCommit(context.TODO(), commit)
+		resp.Error = commitResp.Error
+		resp.RegionError = commitResp.RegionError
+	}
+
+	return resp, nil
 }
 
 // SQL push down commands.
--- kv/transaction/mvcc/scanner.go
+++ kv/transaction/mvcc/scanner.go
@@ -1,24 +1,54 @@
 package mvcc
 
+import (
+	"bytes"
+
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
+)
+
 // Scanner is used for reading multiple sequential key/value pairs from the storage layer. It is aware of the implementation
 // of the storage layer and returns results suitable for users.
 // Invariant: either the scanner is finished and cannot be used, or it is ready to return a value immediately.
 type Scanner struct {
 	// Your Data Here (4C).
+	txn  *MvccTxn
+	iter engine_util.DBIterator
 }
 
 // NewScanner creates a new scanner ready to read from the snapshot in txn.
 func NewScanner(startKey []byte, txn *MvccTxn) *Scanner {
 	// Your Code Here (4C).
-	return nil
+	iter := txn.Reader.IterCF(engine_util.CfWrite)
+	iter.Seek(EncodeKey(startKey, txn.StartTS))
+	return &Scanner{
+		txn:  txn,
+		iter: iter,
+	}
 }
 
 func (scan *Scanner) Close() {
 	// Your Code Here (4C).
+	scan.iter.Close()
 }
 
 // Next returns the next key/value pair from the scanner. If the scanner is exhausted, then it will return `nil, nil, nil`.
 func (scan *Scanner) Next() ([]byte, []byte, error) {
 	// Your Code Here (4C).
-	return nil, nil, nil
+	if !scan.iter.Valid() {
+		return nil, nil, nil
+	}
+	item := scan.iter.Item()
+	key := DecodeUserKey(item.Key())
+	value, _ := item.Value()
+	// 一个key对应多个操作,只取最新的一次,丢弃掉旧操作
+	for ; scan.iter.Valid() && (bytes.Equal(DecodeUserKey(scan.iter.Item().Key()), key) || decodeTimestamp(scan.iter.Item().Key()) > scan.txn.StartTS); scan.iter.Next() {
+	}
+	write, _ := ParseWrite(value)
+
+	if write.Kind == WriteKindDelete {
+		return key, nil, nil
+	}
+
+	value, _ = scan.txn.Reader.GetCF(engine_util.CfDefault, EncodeKey(key, write.StartTS))
+	return key, value, nil
 }
